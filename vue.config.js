ef21f980c0cc593eb78fd22c684c8c50b1228465
Tintinya <1366610224@qq.com>
2019-10-16 10:35:59 +0800

    '初始化项目'

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	configureWpack:{
+		resolve:{
+			alias:{
+				'assest':'@/assets',
+				'common':'@/common',
+				'components':'@/components',
+				'network':'@/network',
+				'views':'@/views',
+				
+			}
+		}
+	}
+}
